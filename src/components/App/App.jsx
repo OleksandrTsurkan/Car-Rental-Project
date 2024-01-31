ff3539b77dc5193a8536bb7a0358aa8bd00886d6
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-31 22:30:28 +0200

    App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,29 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAdverts, fetchFavorites } from 'redux/operations';
+
+const Home = lazy(() => import('pages/Home'));
+const Favorites = lazy(() => import('pages/Favorites'));
+const Advertisement = lazy(() => import('pages/Advertisement'));
+
 export const App = () => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAdverts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchFavorites());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes location={location} key={location.pathname}>
+      <Route path="/" element={<Home />} />
+      <Route path="/advertisement" element={<Advertisement />}></Route>
+      <Route path="/advertisement/favorites" element={<Favorites />} />
+    </Routes>
   );
 };
