a7890d557c021ae21d0a0d37a0b1c70adb27cde4
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-01-31 14:51:46 +0200

    start-redux

---
+++ src/redux/initialState.js
@@ -0,0 +1,13 @@
+export const advertsInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+export const filtersInitialState = '';
+
+export const favoritesInitialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,32 @@
-import { createStore } from './redux/store';
-import rootReducer from './reducers'; 
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { reducer } from './reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
