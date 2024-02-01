88c8781b645b1e9fd354952b2c6852289b7a0fd6
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-02-01 17:47:46 +0200

    components

---
+++ src/components/AdvertisementList/AdvertisementList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { getAdverts, getStatusFilter } from '../../redux/selectors';
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import Card from 'components/Card';
+import { AdvertisementListStyled } from './AdvertisementList.styled';
+
+const AdvertisementList = () => {
+  const adverts = useSelector(getAdverts);
+  const filtered = useSelector(getStatusFilter);
+
+  if (filtered.length === 0) {
+    return (
+      <AdvertisementListStyled>
+        {adverts.map(advertisement => (
+          <Card key={uuidv4()} advertisement={advertisement} />
+        ))}
+      </AdvertisementListStyled>
+    );
+  } else {
+    return (
+      <AdvertisementListStyled>
+        {filtered.map(advertisement => (
+          <Card key={uuidv4()} advertisement={advertisement} />
+        ))}
+      </AdvertisementListStyled>
+    );
+  }
+};
+
+export default AdvertisementList
---
+++ src/components/AdvertisementList/AdvertisementList.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AdvertisementListStyled = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 29px;
+  row-gap: 50px;
+  padding-bottom: 150px;
+`;
